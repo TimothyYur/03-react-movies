a581215f1d49d1cd78d3ddf381de918d6456503b
TimothyY <timjurev@gmail.com>
2026-06-13 15:40:10 +0300

    folder name fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import toast, { Toaster } from 'react-hot-toast';
 import type { Movie } from '../../types/movie';
 import { fetchMovies } from '../../services/movieService';
 import SearchBar from '../SearchBar/SearchBar';
-import MovieGrid from '../MoovieGrid/MoovieGrid';
+import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
